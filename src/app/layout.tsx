7fa3e3f53011c76781bd5a9a109ac6c410435373
morph <duncansantiago18@gmail.com>
2024-03-16 11:28:36 -0400

    Fixed the issue with Nunito font not getting applied on document

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,39 @@
+'use client';
 import { Footer } from '@/components/frames/Footer';
 import { Header } from '@/components/frames/Header';
-import { PLATFORM_PREFIX_NAME, PLATFORM_SUFFIX_NAME } from '@/constants';
-import { Metadata } from 'next/types';
 
 import './globals.css';
+import { Nunito } from 'next/font/google';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-export const metadata: Metadata = {
-	title: `${PLATFORM_PREFIX_NAME} | Dashboard`,
-	description: `${PLATFORM_PREFIX_NAME} ${PLATFORM_SUFFIX_NAME} Dashboard`,
-};
+// export const metadata: Metadata = {
+// 	title: `${PLATFORM_PREFIX_NAME} | Dashboard`,
+// 	description: `${PLATFORM_PREFIX_NAME} ${PLATFORM_SUFFIX_NAME} Dashboard`,
+// };
+
+const entireFont = Nunito({
+	weight: ['200', '300', '400', '700', '900'],
+	subsets: ['latin'],
+});
 
 export default function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const queryClient = new QueryClient();
+
 	return (
-		<main className='bg-bodyBg'>
-			<Header />
-			<div className='w-full mb-[120px] px-[60px]'>{children}</div>
-			<Footer />
-		</main>
+		<html lang='en' suppressHydrationWarning={true}>
+			<body className={entireFont.className}>
+				<QueryClientProvider client={queryClient}>
+					<main className='bg-bodyBg relative'>
+						<Header />
+						<div className='w-full mb-[120px]'>{children}</div>
+						<Footer />
+					</main>
+				</QueryClientProvider>
+			</body>
+		</html>
 	);
 }
