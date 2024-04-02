88296854bcf0815ccb5afb9dcab3275b2e1268d4
morph <duncansantiago18@gmail.com>
2024-04-01 20:03:36 -0400

    added the about section of the job

--- src/components/editor/RichText.tsx
+++ src/components/editor/RichText.tsx
@@ -2,6 +2,10 @@ import ReactHtmlParser from 'react-html-parser';
 
 const RichTexEditor = ({ value }: { value: string }) => {
 	const parsedData = ReactHtmlParser(value);
-	return <div className='w-full h-fit prose lg:prose-xl'>{parsedData}</div>;
+	return (
+		<div className='w-full h-fit prose lg:prose-xl text-bodyText'>
+			{parsedData}
+		</div>
+	);
 };
 export default RichTexEditor;
