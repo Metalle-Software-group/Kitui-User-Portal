d0b0165563fefeed5ae25f04010bf5b9f52af36a
DAMAGER96 <king9jim6@gmail.com>
2024-04-14 17:02:14 +0300

    made header import dynamic

--- src/components/layout-wrappers/index.tsx
+++ src/components/layout-wrappers/index.tsx
@@ -4,33 +4,46 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { usePathname } from 'next/navigation';
 
 import I18nProvider from '@/context/languageContext';
-import { Header } from '../frames/Header';
 import { Footer } from '../frames/Footer';
 import { TNodes } from '@/types/types';
+import dynamic from 'next/dynamic';
+import { Loader } from '../reusables/Others';
+
+const Header = dynamic(
+  () => import('../frames/Header').then((mod) => mod.Header),
+  {
+    ssr: false,
+    loading: (props) => (
+      <div className='w-full h-[200px]'>
+        <Loader />
+      </div>
+    ),
+  }
+);
 
 export default function ({ children }: TNodes) {
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
-	return (
-		<QueryClientProvider client={queryClient}>
-			<I18nProvider>
-				<main className='bg-bodyBg relative'>
-					<div className='hidden md:block'>
-						<Header />
-					</div>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <I18nProvider>
+        <main className='bg-bodyBg relative'>
+          <div className='hidden md:block'>
+            <Header />
+          </div>
 
-					<div
-						className={`w-full h-[calc(100%-108px)] ${
-							!pathname.startsWith('/auth') ? 'mb-[150px] md:mb-[120px]' : ''
-						}`}>
-						{children}
-					</div>
+          <div
+            className={`w-full h-[calc(100%-108px)] ${
+              !pathname.startsWith('/auth') ? 'mb-[150px] md:mb-[120px]' : ''
+            }`}>
+            {children}
+          </div>
 
-					{!pathname.startsWith('/auth') ? <Footer /> : null}
-				</main>
-			</I18nProvider>
-		</QueryClientProvider>
-	);
+          {!pathname.startsWith('/auth') ? <Footer /> : null}
+        </main>
+      </I18nProvider>
+    </QueryClientProvider>
+  );
 }
