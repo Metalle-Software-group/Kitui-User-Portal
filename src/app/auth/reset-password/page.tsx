762341c763505b4a3941348813738b1555a9ea00
morph <duncansantiago18@gmail.com>
2024-04-18 21:05:11 -0400

    Added the set password page

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,17 +1,17 @@
 import { Metadata } from 'next';
 
-import { ResetPassword } from '@/components/auth/Auth';
 import { PLATFORM_PREFIX_NAME, PLATFORM_SUFFIX_NAME } from '@/constants';
+import { AuthPasswordWrapper } from '@/components/reusables/Others';
 
 export const metadata: Metadata = {
-  title: `${PLATFORM_PREFIX_NAME} | Reset password`,
-  description: `${PLATFORM_PREFIX_NAME} ${PLATFORM_SUFFIX_NAME} Reset password`,
+	title: `${PLATFORM_PREFIX_NAME} | Reset password`,
+	description: `${PLATFORM_PREFIX_NAME} ${PLATFORM_SUFFIX_NAME} Reset password`,
 };
 
 export default function Home() {
-  return (
-    <div className="w-full h-[100dvh] bg-[url('/images/others/login.svg')] bg-cover bg-no-repeat">
-      <ResetPassword />
-    </div>
-  );
+	return (
+		<div className="w-full h-[100dvh] bg-[url('/images/others/login.svg')] bg-cover bg-no-repeat">
+			<AuthPasswordWrapper />
+		</div>
+	);
 }
